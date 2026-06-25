f5567840d177a09189f575611b30e78ffb1f1e1a
Marina Lacerda <133787989+marinaplacerda@users.noreply.github.com>
2026-06-25 17:31:45 -0300

    Delete database directory

--- database/20260608_add_cliente_id_to_encomendas.sql
+++
@@ -1,4 +0,0 @@
--- Migration: adicionar cliente_id a encomendas e criar chave estrangeira
-ALTER TABLE encomendas
-    ADD COLUMN cliente_id INT NULL AFTER id,
-    ADD CONSTRAINT fk_encomendas_cliente_id FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE SET NULL;
